d90e662ec72849f78625734697e6e5b9b4530ad2
Lucas Weiblen <lucasweiblen@gmail.com>
2014-03-30 19:59:58 -0300

    Finish chapter 6

--- spec/StockSpec.js
+++ spec/StockSpec.js
@@ -6,11 +6,52 @@ describe("Stock", function() {
   });
 
   describe("when fetched", function() {
+    var xhr;
+
     beforeEach(function() {
-      spyOn($, 'getJSON').andCallFake(function(url, callback) {
-        callback({ sharePrice: 20.13 });
-      });
+      var fetchRequest;
+      xhr = sinon.useFakeXMLHttpRequest();
+
+      xhr.onCreate = function (request) {
+        fetchRequest = request;
+      };
+
       stock.fetch();
+
+      fetchRequest.respond(
+        200,
+        { "Content-Type": "application/json" },
+        '{ "sharePrice": 20.13 }'
+        );
+    });
+
+    afterEach(function() {
+      xhr.restore();
+    });
+
+    it("should update its share price", function() {
+      expect(stock.sharePrice).toEqual(20.13);
+    });
+  });
+
+  describe("when fetched dois", function() {
+    var xhr;
+
+    beforeEach(function() {
+      xhr = sinon.fakeServer.create();
+      xhr.respondWith([
+        200,
+        { "Content-Type": "application/json" },
+        '{ "sharePrice": 20.13 }'
+        ]);
+
+      stock.fetch();
+
+      xhr.respond();
+    });
+
+    afterEach(function() {
+      xhr.restore();
     });
 
     it("should update its share price", function() {
